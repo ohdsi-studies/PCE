256f23bf3cd387e60ff652e30c3a47acd9849960
jreps <jreps@its.jnj.com>
2020-10-13 08:52:54 -0400

    initial files
    initial commit

---
+++ R/Mappers.R
@@ -0,0 +1,9 @@
+getMap <- function(model = 'SimpleModel'){
+  if(model == 'SimpleModel'){
+    map <- function(x){1/(1+exp(-(x-0)))} #logistic function with 0 intercept
+  } else {
+    mapLoc <- system.file("settings", paste0(model,'_finalMap.rds' ), package = "PooledCohortvalidation")
+    map <- readRDS(mapLoc)
+  }
+  return(map)
+}
