2435ec4d1ddd39929c6388c6e702e3c888161053
jreps <jreps@its.jnj.com>
2020-10-28 10:35:45 -0400

    Update Aggregate.R

--- R/Aggregate.R
+++ R/Aggregate.R
@@ -5,6 +5,8 @@ getAggregatePerm <- function(outputFolder,cdmDatabaseName){
   
   settings$targetName <- as.character(settings$targetName)
   settings$outcomeName <- as.character(settings$outcomeName )
+  settings$targetId <- as.double(settings$targetId)
+  settings$outcomeId <- as.double(settings$outcomeId)
   settings$model  <- as.character(settings$model)
   settings$cohortName <- as.character(settings$cohortName)
   settings$analysisId <- as.character(settings$analysisId)
@@ -73,17 +75,17 @@ getAggregatePerm <- function(outputFolder,cdmDatabaseName){
     newSet <- c(
       targetId = -1,
       targetName = 'Persons who are statin-risk eligible',
-      outcomeId = oids[i],
+      outcomeId = as.double(oids[i]),
       outcomeName = unique(settings$outcomeName[settings$outcomeId==oids[i]]),
       model = 'combined',
-      modelSettingsId = "1", 
+      modelSettingsId = 1, 
       analysisId = paste0('Analysis_',50+1*i),
       cohortName = "Persons who are statin-risk eligible",
       devDatabase = NA,
       valDatabase = unique(settings$valDatabase),
       modelSettingName = 'combined',
-      populationSettingId = "1",
-      covariateSettingId = "1"
+      populationSettingId = 1,
+      covariateSettingId = 1
     )
     settings <- rbind(settings,newSet)
     
@@ -115,6 +117,18 @@ getAggregatePerm <- function(outputFolder,cdmDatabaseName){
       if(length(q)==99){
         result$performanceEvaluation$calibrationSummary$predictionThreshold <- c(0, q)
       }
+      
+      # update analysisIds
+      result$performanceEvaluation$calibrationSummary$analysisId <- paste0('Analysis_',50+2*i)
+      result$performanceEvaluation$demographicSummary$analysisId <- paste0('Analysis_',50+2*i)
+      result$performanceEvaluation$evaluationStatistics <- as.data.frame(result$performanceEvaluation$evaluationStatistics)
+      result$performanceEvaluation$evaluationStatistics$analysisId <- paste0('Analysis_',50+2*i)
+      result$performanceEvaluation$evaluationStatistics$Value <- as.character(result$performanceEvaluation$evaluationStatistics$Value)
+      result$performanceEvaluation$evaluationStatistics$Value[result$performanceEvaluation$evaluationStatistics$Metric == 'populationSize'] <- nrow(prediction)
+      result$performanceEvaluation$evaluationStatistics <- result$performanceEvaluation$evaluationStatistics[-grep('_',result$performanceEvaluation$evaluationStatistics$Metric),]
+      result$performanceEvaluation$predictionDistribution$analysisId <- paste0('Analysis_',50+2*i)
+      result$performanceEvaluation$thresholdSummary$analysisId <- paste0('Analysis_',50+2*i)
+      
       
       result <- getSurvivialMetrics(plpResult = result, 
                                     recalibrate = F, 
